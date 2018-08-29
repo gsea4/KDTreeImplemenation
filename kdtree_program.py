d881c5fbdba2c27089755c318b28bb1a57cf2e92
Hai H <haihuynhkbn@gmail.com>
2018-08-28 23:57:55 -0500

    KDTree is behaving correctly on most of 2D arrays, still have issues with some of the input that causes infinite recursion

--- kdtree_program.py
+++ kdtree_program.py
@@ -2,29 +2,43 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-arr = np.array([[1,9],[9,3],[4,1],[3,7],[5,4],[6,8],[7,2],[8,8],[7,9],[9,6]])
+# arr = np.array([(2,4), (7,1), (3,8), (6,1), (2,5), (6,6)])
+arr = np.array([(3,5), (3,6), (2,6)])
+print(np.median(arr[:, 0]))
 
-def divide(arr, branch):
-    mid = int(np.ceil(len(arr)/2))
-    left = np.array([[]])
-    right = np.array([[]])
+class KDTree:
+    def __init__(self, matrix, branch):
+        self.left = None
+        self.right = None
+        print("Matrix shape: " + str(matrix.shape[0]))
+               
+        if matrix.shape[0] == 1:
+            # self.node = np.median(matrix[:, branch])
+            self.data = matrix[0]
+            return
+        if matrix.shape[0] == 0:
+            return
+
+        self.node = np.median(matrix[:, branch])
+        print(np.median(matrix[:, branch]))
+
+        left = []
+        right = []
+
+        for i in matrix[:, :]:
+            if i[branch] <= self.node:
+                left.append(i)
+            else:
+                right.append(i)
+
+        self.left = KDTree(np.asarray(left), (branch + 1)%2)
+        self.right = KDTree(np.asarray(right), (branch + 1)%2)
+
     
-    for i in arr[:mid]:
-        if i[branch] < arr[mid,0]:
-            print(i)
-            np.append(left,[i])
-        else:
-            np.append(right, i)
+    def insert_data(self, root, matrix):
+        #if array size is 1 then set data = array[0]
+        #else perform another insert_data
+        pass
 
-    for i in arr[mid:]:
-        if i[branch] >= arr[mid,0]:
-            np.append(right, i)
-
-        else:
-            np.append(left,i)
-
-    print(left)
-    print(right)
-
-
-divide(arr, 0)
+tree = KDTree(arr, 0)
+print("Yo")
---
+++ new_class.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+    
+    def print_data(self):
+        print(self.data)
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n1.print_data()
+
+n1.left = n2
+n2.right = n3
+
+print(n1.left)
+print(n2)
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+#arr = np.array([[(2,4), (7,1), (3,8), (6,1), (2,5), (6,6), (10,9)]])
+#print(arr)
+arr = np.array([(2,4)])
+print(arr.shape[0])
+mid = np.median(arr[:, 0])
+print(np.median(arr[:, 1]))
+# print(arr[:mid, 0])
+# print(np.median(arr))
+
+import numpy as np
+from sklearn.neighbors import KDTree
+arr = np.array([(1,7), (1,6), (6,6), (6,6)])
+new_tree = KDTree(arr, leaf_size = 1)
+print("Done")
+
+import numpy as np
+from scipy.spatial import cKDTree
+#arr = np.array([(1,7), (1,6), (6,6), (6,6)])
+arr = np.array([(6,1), (7,1), (6,6)])
+tree = cKDTree(arr, 1)
+
+print("Something")
